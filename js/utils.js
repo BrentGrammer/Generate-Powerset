6b26d67c4cb14170d37412352870b556f14a44eb
Brent Marquez <brentonmarquez@gmail.com>
2023-01-21 21:11:44 -0700

    remove log

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 const removeLastComma = (str) => {
   let removed = str;
-  console.log(str.slice(-1), 'str')
+ 
   const lastChar = str.slice(-1);
   if (lastChar === ",") {
     removed = str.slice(0, -1);
